06f9118659e4158632375ee0a432429825530feb
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2016-10-14 09:09:54 +0900

    Q1:prediction using Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,5 +29,37 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time()-t0, 3), "s"
 
 
+    ### use the trained classifier to predict labels for the test features
+    t0 = time()
+    pred = clf.predict(features_test)
+    print "prediction time:", round(time()-t0, 3), "s"
+
+
+    ### calculate and return the accuracy on the test data
+    ### this is slightly different than the example,
+    ### where we just print the accuracy
+    ### you might need to import an sklearn module
+    accuracy = clf.score(features_test, labels_test)
+    print accuracy
+    return accuracy
+
+
+def main():
+    NBAccuracy(features_train, labels_train, features_test, labels_test)
+
+if __name__ == "__main__":
+    main()
