b99a617ad0f65bfdc9c4c7f4a8fcbf760f65f98d
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2016-10-18 01:11:26 +0900

    Outliers - Quiz: Enron Outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -6,14 +6,29 @@ import matplotlib.pyplot
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
+import pprint
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+pprint.pprint(data_dict)
+data_dict.pop('TOTAL',0)
+other_4_outliers = [ r for r in data_dict if data_dict[r]["salary"] > 1000000 and data_dict[r]["salary"] != 'NaN' and data_dict[r]["bonus"] > 5000000 ]
+print other_4_outliers
 features = ["salary", "bonus"]
+
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
 
